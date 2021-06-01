14ad41e29e4a6b71345da84f70e3d8e093d9dbdb
thedevtechs <thedevtechs@gmail.com>
2021-05-31 21:51:40 -0700

    Updating styles and rearranging components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Homepage from "./components/homepage/homepage.component";
+import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
       <Homepage/>
-
-
-
     </div>
   );
 }
